694f36df4ab366abaaef120d1f031fd87d41c061
Jared (Desktop) <jemraider21@gmail.com>
2019-03-11 19:46:34 -0400

    Created Javadoc for Lab3 and preped Lab3C and Customers

---
+++ Customers.java
@@ -0,0 +1,5 @@
+public class Customers{
+
+    // Start program
+
+} // End Customers class
---
+++ Lab3C.java
@@ -0,0 +1,24 @@
+/**
+ * HCC Honey Inc wants to begin collecting names for a monthly mailing list. I am tasked with creating a program to process the information
+ * There will be a menue that asks the user what they want to do:
+ *      1.) Add New Entry: Prompts the user for customer information and add it to the customer list
+ *      2.) Remove Entry: Prompts the user for a last name, then removes any matching customers from the list
+ *      3.) Sort by Zip Code
+ *      4.) Sort by State
+ *      5.) Search by Name
+ *      6.) Print Customers
+ *      7.) Quit
+ */
+
+
+import java.util.Scanner;
+
+public class Lab3C{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+    } // End main method
+
+} // End Lab3C class
