10db5f5d71af54b6117d0e21b2e618581711db06
Jared (Desktop) <jemraider21@gmail.com>
2019-03-11 20:18:00 -0400

    Created two versions of the array and created an outline for Lab3C

--- Lab3C.java
+++ Lab3C.java
@@ -18,6 +18,85 @@ public class Lab3C{
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
+        
+        // One version of the array
+        String [][] customerInfoA = {
+
+            {"Name",            "Ansel Carter",       "Darby Hamsandwich",    "Ally Gator",      "Amanda Huginkiss",    "Franken Stein"},
+            {"Address",         "8397 Zip Rd",        "147 LedStock Ave",     "34 Main St",      "1222 Mover Rd",       "190 Princeton Ct"},
+            {"City",            "Ellicott Chance",    "Bloomneld",            "Bloomneld",       "Tulsom",              "Briggon"},
+            {"State",           "MD",                 "NJ",                   "NJ",              "CA",                  "MI"},
+            {"Zip Code",        "21999",              "07001",                "07001",           "90001",               "48003"},
+            {"Phone Number",    "123-456-7890",       "890-123-4567",         "456-789-0123",    "345-678-9012",        "678-901-2345"}
+
+        };
+
+        // Second version of array
+        String [][] customerInfoB = {
+
+            {"Name",                 "Address",             "City",               "State",    "Zip Code",    "Phone Number"},
+            {"Ansel Carter",         "8397 Zip Rd",         "Ellicott Chance",    "MD",       "21999",       "123-456-7890"},
+            {"Darby Hamsandwich",    "147 LedStock Ave",    "Bloomneld",          "NJ",       "07001",       "890-123-4567"},
+            {"Ally Gator",           "34 Main St",          "Bloomneld",          "NJ",       "07001",       "456-789-0123"},
+            {"Amanda Huginkiss",     "1222 Mover Rd",       "Tulsom",             "CA",       "90001",       "345-678-9012"},
+            {"Franken Stein",        "190 Princeton Ct",    "Briggon",            "MI",       "48003",       "678-901-2345"},
+
+        };
+        
+        // Welcomes the user to the program
+
+        // Create a while loop for user input for the provided menue
+
+            // Display the Menue
+            //Ask the user to make a selection
+            
+            // If user selects 1
+
+                // Ask the user for information
+                // Add information to the list
+
+            // If user selects 2
+
+                // Prompt the user for a last name
+                // Remove any matching custoemrs from the list
+
+            // If user selects 3
+
+                // Ask the user to select if they want the names sorted from 00000 - 99999
+                // If user selects 1
+                    // Print the information with names sorted from 00000 - 99999
+                // If user selects 2
+                    // Print the information with names sorted from 99999 - 00000
+
+            // If user selects 4
+
+                // Ask the user to select if they want the states sorted from Al - Wy
+                // If user selects 1
+                    // Print the information with names sorted from Al - Wy
+                // If user selects 2
+                    // Print the information with names sorted from Wy - Al
+
+            // If user selects 5
+
+                // Ask the user to select if they want the names sorted from A-Z or Z-A
+                // If user selects 1
+                    // Print the information with names sorted from A-Z
+                // If user selects 2
+                    // Print the information with names sorted from Z-A
+
+            // If user selects 6
+
+                // Print out a list of all the customers and their information
+
+            // If user selects 7
+
+                // Break the loop
+
+            // End of user choice if statements
+
+        // End of menue while loop
+
+        // Say goodbye to the customer
 
     } // End main method
 
